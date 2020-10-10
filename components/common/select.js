edcde96a5c586d75267c3581c1110703650709f1
Akshat <akshatbhargavadeepa@gmail.com>
2020-10-10 15:27:14 +0530

    feat: 🎸 developed custom-select component

---
+++ components/common/select.js
@@ -0,0 +1,45 @@
+import { Button, RadioButtonGroup } from "@chakra-ui/core";
+
+const CustomRadio = React.forwardRef((props, ref) => {
+  const { isChecked, isDisabled, value, ...rest } = props;
+  return (
+    <Button
+      ref={ref}
+      variantColor={isChecked ? "blue" : "gray"}
+      aria-checked={isChecked}
+      role="radio"
+      isDisabled={isDisabled}
+      {...rest}
+    />
+  );
+});
+
+const Select = ({
+  placeholder,
+  defaultValue,
+  options,
+  onChange,
+  inline = true,
+  className = '',
+}) => {
+	return (
+    <RadioButtonGroup
+			isInline={inline}
+      onChange={onChange}
+      placeholder={placeholder}
+      defaultValue={defaultValue}
+      className={className}
+    >
+      {options.map((option, key) => (
+				<CustomRadio
+					key={key}
+					value={option.value}
+				>
+					{option.label}
+				</CustomRadio>
+			))}
+    </RadioButtonGroup>
+	);
+};
+
+export default Select;
